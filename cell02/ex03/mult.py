de5924e988fbce1e11ef5fb0b799e2d9eaabaa36
65050678-bambam <65050678@kmitl.ac.th>
2025-09-16 19:55:01 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,11 @@
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+result = number1 * number2
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is zero.")
+
+print("The multiplication result is:", result)

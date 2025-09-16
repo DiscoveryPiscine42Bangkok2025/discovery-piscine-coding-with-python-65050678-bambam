a419ffa33eae58a8e048e6c5f796a9f27c93cccf
65050678-bambam <65050678@kmitl.ac.th>
2025-09-16 19:53:54 +0700

    Add files via upload

---
+++ cell02/ex02/password.py
@@ -0,0 +1,6 @@
+correct_password = "abcd01_23"
+entered_password = input("Enter the password: ")
+if entered_password == correct_password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
